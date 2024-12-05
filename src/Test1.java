da8ebb74c0157e9f25e761b365285ade3c032fb3
Sri <sriharan.selvaganapathy@sandhata.com>
2024-12-05 12:49:39 +0530

    Initial Commit

---
+++ src/Test1.java
@@ -0,0 +1,9 @@
+public class Test1 {
+    public static void main(String[] args){
+        int num1=1;
+        int num2 = num1+1;
+        //num1++;
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
